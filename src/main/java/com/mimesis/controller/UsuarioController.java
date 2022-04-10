fa1e28c0c7d3d6f407c5cab4be9d89fe09e71aee
Diego Vargas <a20191027@pucp.edu.pe>
2022-04-10 13:03:31 -0500

    logos

--- src/main/java/com/mimesis/controller/UsuarioController.java
+++ src/main/java/com/mimesis/controller/UsuarioController.java
@@ -1,4 +1,14 @@
 package com.mimesis.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
 public class UsuarioController {
+    @GetMapping("")
+    public String paginaPrincipal(){
+        return "usuario/main";
+    }
 }
