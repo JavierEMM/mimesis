eb47508f8248d770477026783af043e5f63e4733
Stephania <stephania.guzman@pucp.edu.pe>
2022-04-24 17:23:12 -0500

    agregar class

--- src/main/java/com/mimesis/controller/OperadorController.java
+++ src/main/java/com/mimesis/controller/OperadorController.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/operador")
@@ -69,6 +71,34 @@ public class OperadorController {
         funcionRepository.save(funcion);
         return  "redirect:/operador";
     }
+
+   /* @GetMapping("/edit")
+    public String editarFuncion(Model model, @RequestParam("id") int id, @ModelAttribute("employees") Employees employees, RedirectAttributes redirectAttributes) {
+        Optional<Employees> employeesOptional = employeesRepository.findById(id);
+        if (employeesOptional.isPresent()) {
+            employees = employeesOptional.get();
+            Employees emplo = new Employees();
+            if((emplo = employees.getManagerid()) == null){
+                redirectAttributes.addFlashAttribute("nohayjefe", "No puedes editar a este usuario. MANAGER_ID = NULL");
+                return "redirect:/employee";
+            }
+            model.addAttribute("employees", employees);
+            model.addAttribute("listaDepartaments", departmentsRepository.findAll());
+            List<Departments> departmentOpt = departmentsRepository.findAll();
+            List<Departments> departamentosFinales = new ArrayList<Departments>();
+            for (Departments i : departmentOpt){
+                if(i.getManagerid() != null){
+                    departamentosFinales.add(i);
+                }
+            }
+            model.addAttribute("listaJobs", jobsRepository.findAll());
+            model.addAttribute("listaDepartamentosconJefes", departamentosFinales);
+            return "employee/Frm";
+        } else {
+            return "redirect:/employee";
+        }
+
+    }*/
 
 
 }
