40589dfd77687e909c94b030a3359bac2b279785
NicoleG <guillen.nicole@pucp.edu.pe>
2022-05-31 18:58:47 -0500

    Obras

--- src/main/java/com/mimesis/repository/ObrasRepository.java
+++ src/main/java/com/mimesis/repository/ObrasRepository.java
@@ -13,7 +13,7 @@ public interface ObrasRepository extends JpaRepository<Obra,Integer> {
     @Query(value="SELECT o.* FROM funcion f inner join obras o on f.obras_idobras = o.idobras where o.nombre like %?1% ;",nativeQuery = true)
     List<Obra> listaBuscarObrasNombre(String busqueda);
 
-    @Query(value="SELECT o.* FROM obras o inner join genero  g on o.genero_idgenero = g.idgenero where o.nombre like %?1% limit 6",nativeQuery = true)
+    @Query(value="SELECT o.* FROM obras o inner join genero  g on o.genero_idgenero = g.idgenero where o.nombre like %?1%",nativeQuery = true)
     List<Obra> listaBuscarObrasUnicasPorNombre(String busqueda);
 
     @Query(value="SELECT o.* FROM funcion f inner join obras o on f.obras_idobras = o.idobras inner join genero  g on o.genero_idgenero = g.idgenero where g.nombre like %?1% ;",nativeQuery = true)
