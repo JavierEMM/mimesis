217095ccf28214f883fb646d3dbb1e70be3b9360
NicoleG <guillen.nicole@pucp.edu.pe>
2022-05-31 03:24:34 -0500

    editar_perfil_si_funciona

--- src/main/java/com/mimesis/controller/UsuarioController.java
+++ src/main/java/com/mimesis/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.mimesis.controller;
 
+import com.mimesis.dto.DTOcarrito;
 import com.mimesis.entity.Usuario;
 import com.mimesis.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Optional;
 
 @Controller
@@ -38,26 +42,37 @@ public class UsuarioController {
     public String perfil(){
         return "usuario/perfil";
     }
+    @GetMapping("/perfil1")
+    public String perfil1(Model model, @RequestParam(value = "id",required = false) Integer id){
+        System.out.println(id);
+        model.addAttribute("lista",usuarioRepository.findById(id));
+        return "usuario/perfil";
+    }
+
 
     @PostMapping("/perfil/save")
     public String guardarPerfil(@RequestParam("archivo")MultipartFile file, Usuario usuario,
                                 @RequestParam("direccion")String direccion,
-                                @RequestParam("dni")Integer dni,
-                                Model model){
+                                @RequestParam("numerotelefonico")String tel,
+                                Model model, HttpSession session){
 
         if(file.isEmpty()){
             model.addAttribute("msg", "Debe subir una imagen");
-            return "usuario/perfil";
+            return "usuario/editarperfil";
         }
+
 
         try{
             usuario.setFotoperfil(file.getBytes());
             usuario.setDireccion(direccion);
            // usuario.setFechanacimiento(fechanacimiento);
-            usuario.setDni(dni);
+            usuario.setNumerotelefonico(tel);
 
             usuarioRepository.save(usuario);
-            return "usuario/perfil";
+            session.setAttribute("usuario",usuario);
+            Integer usuario1 = usuario.getId();
+
+            return "redirect:/perfil1?id="+usuario1;
         } catch (IOException e){
             e.printStackTrace();
             model.addAttribute("msg", "ocurrio un error al subir el archivo");
@@ -66,7 +81,7 @@ public class UsuarioController {
     }
 
     @GetMapping("/perfil/editar")
-    public String editarPerfil(@RequestParam("id")Integer id, Model model, RedirectAttributes attributes){
+    public String editarPerfil(@RequestParam("id")Integer id, Model model){
 
         Optional<Usuario> user = usuarioRepository.findById(id);
         model.addAttribute("usuario", user.get());
@@ -74,17 +89,29 @@ public class UsuarioController {
     }
 
     @GetMapping("image/{id}")
-    public ResponseEntity<byte[]> mostrarImagen(@PathVariable("id") int id){
+    public ResponseEntity<byte[]> mostrarImagen(@PathVariable("id") int id,HttpSession session){
         Optional<Usuario> opt = usuarioRepository.findById(id);
+        System.out.println(opt);
+        System.out.println("aqui");
+        System.out.println(id);
         if(opt.isPresent()){
             Usuario u = opt.get();
+
             byte[] imagenComoBytes = u.getFotoperfil();
+            System.out.println(imagenComoBytes);
+            System.out.println(id);
             return new ResponseEntity<>(imagenComoBytes, HttpStatus.OK);
         }else{
             return null;
         }
     }
 
+    @GetMapping("perfil/google")
+    public String perfilGoogle(){
+
+
+        return "usuario/perfilprimeravez";
+    }
 
 
     @GetMapping("/historial")
