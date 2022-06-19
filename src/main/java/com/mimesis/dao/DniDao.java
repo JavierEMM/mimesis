7e43f08670b11b226216c5cb7c3251ea9ed01a8d
Joau0824 <a20182162@pucp.edu.pe>
2022-06-19 17:51:55 -0500

    dni push 1

--- src/main/java/com/mimesis/dao/DniDao.java
+++ src/main/java/com/mimesis/dao/DniDao.java
@@ -9,12 +9,19 @@ import java.util.List;
 
 public class DniDao {
 
-    public List<DTODni> listarProductos(String dni) {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<DTODni[]> response = restTemplate.getForEntity(
-                "https://dniruc.apisperu.com/api/v1/dni/"+ dni +"?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IlRlYXRyb3NNaW1lc2lzMUBnbWFpbC5jb20ifQ.iVMwZdyRDDp0P4a3q1Jqn0fwgiIJgxvwNVSStolKK7Y", DTODni[].class);
-
-        return Arrays.asList(response.getBody());
+    public Boolean ConsultarDNI(DTODni dni) {
+        try{
+            RestTemplate restTemplate = new RestTemplate();
+            String url = "https://dniruc.apisperu.com/api/v1/dni/"+ dni.getDni() +"?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IlRlYXRyb3NNaW1lc2lzMUBnbWFpbC5jb20ifQ.iVMwZdyRDDp0P4a3q1Jqn0fwgiIJgxvwNVSStolKK7Y";
+            ResponseEntity<DTODni> responseMap = restTemplate.getForEntity(url, DTODni.class);
+            DTODni dtoDni = responseMap.getBody();
+            System.out.println(dtoDni);
+            System.out.println("TRY");
+            return true;
+        }catch (Exception e){
+            System.out.println("CATCH");
+            return false;
+        }
     }
 
 }
