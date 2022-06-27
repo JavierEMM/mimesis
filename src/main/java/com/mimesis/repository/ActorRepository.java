daa5c8e241378d5ea5f89fdaf99917e2460f5df3
Stephania <stephania.guzman@pucp.edu.pe>
2022-06-26 19:14:19 -0500

    ActorRepository

--- src/main/java/com/mimesis/repository/ActorRepository.java
+++ src/main/java/com/mimesis/repository/ActorRepository.java
@@ -11,21 +11,21 @@ import java.util.List;
 
 @Repository
 public interface ActorRepository extends JpaRepository<Actor,Integer> {
-    @Query(nativeQuery = true, value = "SELECT  * FROM actor WHERE concat(nombre,' ',apellido) like %?1%;")
+    @Query(nativeQuery = true, value = "SELECT  * FROM actor WHERE concat(nombre,' ',apellido) like %?1%")
     List<Actor> busquedaActor(String nombre);
 
     @Query(nativeQuery = true, value = "SELECT  * FROM actor WHERE nombre like %?1%")
     List<Actor> busquedaActorporNombre(String nombre);
 
-    @Query(value = "select idactor from calificaciones where idactor is not null;",nativeQuery = true)
+    @Query(value = "select idactor from calificaciones where idactor is not null",nativeQuery = true)
     List<Integer> obtenerIdCalificacion();
 
     @Query(nativeQuery = true, value = "SELECT a.nombre, a.apellido, avg(c.calificacion) as calificacion  FROM mimesis.calificaciones  c \n" +
             "inner join actor a on a.idactor=c.idactor \n" +
-            "where c.idactor IS NOT NULL group by c.idactor;")
+            "where c.idactor IS NOT NULL group by c.idactor")
     List<DTOActoresMejoresCalificados> obtenerActoresMejoresCalificados();
 
-    @Query(nativeQuery = true, value = "SELECT * FROM actor a WHERE a.nombre or a.apellido like %?1%;")
+    @Query(nativeQuery = true, value = "SELECT * FROM actor a WHERE a.nombre or a.apellido like %?1%")
     List<Actor> busquedaBestoAct(String busqueda);
 
 }
